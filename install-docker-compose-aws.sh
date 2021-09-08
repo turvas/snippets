551351a21b2b97d531788561986c36a14a2ba65c
kalev kuusik <kalev@bytelife.ee>
2021-09-08 13:01:24 +0000

    docker-compose version pto 1.29

--- install-docker-compose-aws.sh
+++ install-docker-compose-aws.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # https://acloudxpert.com/how-to-install-docker-compose-on-amazon-linux-ami/
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
